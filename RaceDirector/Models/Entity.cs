7204c951f35b1a97082f52fd281afdf2cf9e9523
Alexandr Grebenkov <alexandrgrebenkov@gmail.com>
2019-08-07 17:53:07 +0500

    Add project files.

---
+++ RaceDirector/Models/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace RaceDirector.Models
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ RaceDirector/Models/Race.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaceDirector.Models
+{
+    public class Race : Entity
+    {
+    }
+}
