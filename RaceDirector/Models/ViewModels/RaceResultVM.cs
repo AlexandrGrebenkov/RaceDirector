1fdeaacd47c463dc9b5a3e793d778b99e476869f
Alexandr Grebenkov <alexandrgrebenkov@gmail.com>
2019-08-07 18:32:26 +0500

    ViewModels

---
+++ RaceDirector/Models/ViewModels/RaceResultVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RaceDirector.Models.ViewModels
+{
+    public class RaceResultVM
+    {
+        public int Id { get; set; }
+
+        public string TrackName { get; set; }
+        public string RaceClassName { get; set; }
+        public string RaceDate { get; set; }
+
+        public string DriverFullName { get; set; }
+
+        /// <summary>Занятое место</summary>
+        public int Position { get; set; }
+
+        /// <summary>Количество кругов</summary>
+        public double Laps { get; set; }
+
+        /// <summary>Время в квалификации</summary>
+        public TimeSpan LapTime { get; set; }
+
+        /// <summary>Лучшее время в гонке</summary>
+        public TimeSpan BestTime { get; set; }
+    }
+}
---
+++ RaceDirector/Models/ViewModels/RaceVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RaceDirector.Models.ViewModels
+{
+    public class RaceVM
+    {
+        public int Id { get; set; }
+
+        public string TrackName { get; set; }
+        public string RaceClassName { get; set; }
+        public string RaceDate { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public IEnumerable<RaceResultVM> Results { get; set; }
+    }
+}
