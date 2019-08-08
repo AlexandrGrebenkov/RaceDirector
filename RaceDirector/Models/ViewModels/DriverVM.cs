133f1429a23c0431fdc7056a53caddd5ce62e071
Alexandr Grebenkov <alexandrgrebenkov@gmail.com>
2019-08-08 22:57:36 +0500

    1

---
+++ RaceDirector/Models/ViewModels/DriverVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RaceDirector.Models.ViewModels
+{
+    public class DriverVM
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age => (new DateTime(1, 1, 1) + (DateTime.Now.Date - DateOfBirth)).Year - 1;
+    }
+}
