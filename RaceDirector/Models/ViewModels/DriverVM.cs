473fca883534c6190afa76482b49216428b3f91a
Alexandr Grebenkov <alexandrgrebenkov@gmail.com>
2019-08-10 12:17:23 +0500

    Driver views

--- RaceDirector/Models/ViewModels/DriverVM.cs
+++ RaceDirector/Models/ViewModels/DriverVM.cs
@@ -7,7 +7,20 @@ namespace RaceDirector.Models.ViewModels
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string FullName => $"{LastName} {FirstName}";
         public DateTime DateOfBirth { get; set; }
         public int Age => (new DateTime(1, 1, 1) + (DateTime.Now.Date - DateOfBirth)).Year - 1;
+        public string AgeString
+        {
+            get
+            {
+                string str = "лет";
+                if (Age % 10 <= 4)
+                    str = "года";
+                if (Age % 10 == 1)
+                    str = "год";
+                return $"{Age} {str}";
+            }
+        }
     }
 }
